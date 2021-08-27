cd3a4604ad0fcd7ebcbeea466b2b32e37660cb85
r03ert0 <robertotoro@gmail.com>
2021-08-27 20:22:29 +0200

    fix cloud code

--- server/cloud/main.js
+++ server/cloud/main.js
@@ -1,12 +1,12 @@
 // Cloud Code entry point
+
 var request = require('request-promise');
 var solrURL = "http://localhost:8983/solr/brainspell/select";
 Parse.Cloud.define("select", async (req) => {
-  const qs = (({ q, fl, start, rows}) => ({ q, fl, start, rows }))(req.params);
-  const res = await request({
-    url: solrURL,
-    qs: qs
-  });
-
-  return JSON.parse(res).response;
+    const qs = (({ q, fl, defType, qf, start, rows}) => ({ q, fl, defType, qf, start, rows }))(req.params);
+    let res = await request({
+        url: solrURL,
+        qs: qs
+    });
+    return JSON.parse(res).response;
 });
